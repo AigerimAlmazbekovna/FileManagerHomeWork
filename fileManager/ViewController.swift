a70e3ffd30ac4b5a39876e316e902a52acdf18a2
Aigerim Almazbekova <aigerimalmazbekovna1416@gmail.com>
2024-09-12 13:56:29 +0500

    Initial Commit

---
+++ fileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fileManager
+//
+//  Created by Айгерим on 12.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
